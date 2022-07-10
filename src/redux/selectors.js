c96aaea3cc8d3392405e38f1c02531d6c7f7fd43
Pavel Ksenzhik <ksyonzhik2000@mail.ru>
2022-07-10 22:47:26 +0300

    Fix bag with abbr-picker__items

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -8,7 +8,7 @@ export const prevCurrAmountSelector = (state) => state.prevData.prevCurrAmount;
 export const ratesLoadingSelector = (state) => state.rates.loading;
 export const ratesLoadedSelector = (state) => state.rates.loaded;
 export const ratesErrorSelector= (state) => state.rates.error;
-export const activeRatesSelector = (state) => Object.values(ratesSelector(state)[state.day] || 0);
+export const activeRatesSelector = (state) => Object.values(ratesSelector(state)[state.prevData.day] || 0);
 
 export const activeAbbrSelector = (state) => state.currency.Cur_Abbreviation;
 export const activeNameSelector = (state) => state.currency.Cur_Name;
