be734606046988539253d61603c6267879e3e193
Pavel Ksenzhik <ksyonzhik2000@mail.ru>
2022-07-14 16:53:46 +0300

    Renamed activeField

--- src/components/countPicker/countPicker.js
+++ src/components/countPicker/countPicker.js
@@ -1,13 +1,13 @@
 import './countPicker.scss'
 
 
-function CountPicker({ value, setter, field, setActiveField }) {
+function CountPicker({ value, setter, curr, setActiveCurr }) {
     return (
         <div className="count-picker">
             <input className="count-picker__input" type='number' value={value} 
                 onChange={(event) => {
                     setter(event.target.value);
-                    setActiveField(field);
+                    setActiveCurr(curr);
                 }}
             />
         </div>
